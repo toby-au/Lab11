c096890508c4b5f1abbb73c291fa8788f1099150
Toby <31707836+toby-au@users.noreply.github.com>
2018-02-02 10:36:42 -0500

    Update poke-lighting.py

--- poke-lighting.py
+++ poke-lighting.py
@@ -1,7 +1,7 @@
 """
 Toby Au
 CSC1 141
-Lab 11: Pokemon: Lightning Round
+Pokemon: Lightning Round
 Pokemon Card game: each player gets a random deck of 15 cards, select which strategy to use and returns the winners ID
 """
 
